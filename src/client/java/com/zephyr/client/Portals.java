71b9f565341385ff62d6238a3e51715fbb2792af
alderjhuynh <alderjhuynh@gmail.com>
2026-04-09 19:22:55 -0700

    Allowed gui opening inside portals

---
+++ src/client/java/com/zephyr/client/Portals.java
@@ -0,0 +1,5 @@
+package com.zephyr.client;
+
+public class Portals {
+    public static boolean enabled = true;
+}
---
+++ src/client/java/com/zephyr/client/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,18 @@
+package com.zephyr.client.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.zephyr.client.Portals;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+    @ModifyExpressionValue(method = "tickNausea", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;currentScreen:Lnet/minecraft/client/gui/screen/Screen;", opcode = Opcodes.GETFIELD))
+    private Screen modifyNauseaCurrentScreen(Screen original) {
+        if (Portals.enabled) return null;
+        return original;
+    }
+}
