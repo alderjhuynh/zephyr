7133deb90a0ee8057e8552fa2f4ccdbc1ceb34a9
alderjhuynh <alderjhuynh@gmail.com>
2026-08-12 13:24:37 -0700

    Delete src directory

--- src/client/java/com/zephyr/client/module/qol/appleskin/IntPoint.java
+++
@@ -1,7 +0,0 @@
-package com.zephyr.client.module.qol.appleskin;
-
-/** Mutable integer point, reused by the HUD offset caches to avoid allocation. */
-public class IntPoint {
-    public int x;
-    public int y;
-}
